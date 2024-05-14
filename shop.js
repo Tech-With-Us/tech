5bd2b735bccde43684dbfbeb9fb4d39d7669fa12
Tech-With-Us <techwithus000@gmail.com>
2024-05-14 15:52:47 +0530

    Update shop.js

--- shop.js
+++ shop.js
@@ -197,27 +197,35 @@ function searchWork() {
                 prLink = prList[item[1]][3]
                 prDisc = prList[item[1]][4]
 
-                let innerCon = `<div class="image-container" id="${targetId}">
-        </div>
-                    <div class="product-details">
-                        <div><h5>${prName}</h5><span id="${idName}" >${prType}</span></div>
-                        <u>${prProduct}</u>
+                let innerCon = `
+                     <div class="image-container" id="${targetId}"></div>
+
+            <div class="product-details-container">
+                <div class="product-details">
+                    <div>
+                        <h5>${prName}</h5><span id="${idName}">${prType}</span>
                     </div>
+                    <u>${prProduct}</u>
+                </div>
+                <div class="product-buy-disc">
                     <p>${prDisc}
                     </p>
                     <h4><a class="button" href="${prLink}" target="_blank">
-<div class="button-wrapper">
-  <div class="text">Buy Now</div>
-    <span class="shop-icon">
-      <svg viewBox="0 0 16 16" class="bi bi-cart2" fill="currentColor" height="16" width="16" xmlns="http://www.w3.org/2000/svg">
-  <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"></path>
-</svg>
-    </span>
-  </div>
-</a>
-
-</h4>
-                    </div>
+                            <div class="button-wrapper">
+                                <div class="text">Buy Now</div>
+                                <span class="shop-icon">
+                                    <svg viewBox="0 0 16 16" class="bi bi-cart2" fill="currentColor" height="16"
+                                        width="16" xmlns="http://www.w3.org/2000/svg">
+                                        <path
+                                            d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z">
+                                        </path>
+                                    </svg>
+                                </span>
+                            </div>
+                        </a>
+                    </h4>
+                </div>
+            </div>
                     `
                 let div = document.createElement('div')
                 div.classList.add('product')
@@ -377,27 +385,34 @@ function show(id) {
         prImages = element[2]
         prLink = element[3]
         prDisc = element[4]
-        let innerCon = `<div class="image-container" id="${targetId}">
-        </div>
-                    <div class="product-details">
-                        <div><h5>${prName}</h5><span id="${idName}">${prType}</span></div>
-                        <u>${prProduct}</u>
+        let innerCon = ` <div class="image-container" id="${targetId}"></div>
+
+            <div class="product-details-container">
+                <div class="product-details">
+                    <div>
+                        <h5>${prName}</h5><span id="${idName}">${prType}</span>
                     </div>
+                    <u>${prProduct}</u>
+                </div>
+                <div class="product-buy-disc">
                     <p>${prDisc}
                     </p>
                     <h4><a class="button" href="${prLink}" target="_blank">
-<div class="button-wrapper">
-  <div class="text">Buy Now</div>
-    <span class="shop-icon">
-      <svg viewBox="0 0 16 16" class="bi bi-cart2" fill="currentColor" height="16" width="16" xmlns="http://www.w3.org/2000/svg">
-  <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"></path>
-</svg>
-    </span>
-  </div>
-</a>
-
-</h4>
-                    </div>
+                            <div class="button-wrapper">
+                                <div class="text">Buy Now</div>
+                                <span class="shop-icon">
+                                    <svg viewBox="0 0 16 16" class="bi bi-cart2" fill="currentColor" height="16"
+                                        width="16" xmlns="http://www.w3.org/2000/svg">
+                                        <path
+                                            d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z">
+                                        </path>
+                                    </svg>
+                                </span>
+                            </div>
+                        </a>
+                    </h4>
+                </div>
+            </div>
                     `
         let div = document.createElement('div')
         div.classList.add('product')
